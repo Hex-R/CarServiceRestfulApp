4321b28301a809241c7467d29ec52ebd6ca8fd7a
Hex <hex.raeder@gmail.com>
2022-04-07 18:27:32 +0300

    Initial commit

---
+++ src/main/java/com/hex/car_service_restful_app/CarServiceRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.hex.car_service_restful_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarServiceRestfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hex/car_service_restful_app/CarServiceRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hex.car_service_restful_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
